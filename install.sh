1f323be0e19e6151cdee526cc91be43ad123f7e1
Kronosol <45532845+blipk@users.noreply.github.com>
2022-12-29 16:15:28 +1100

    XDG install

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir -p /opt/
+git clone https://github.com/RobinLinus/snapdrop.git /opt/
+yes | cp -f ./xdg/icons/*.png ~/.local/share/icons
+yes | cp -f ./xdg/*.desktop ~/.local/share/applications
